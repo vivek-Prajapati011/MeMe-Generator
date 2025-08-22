b1301e9170462195da162fdb98c6da9313cc9df4
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-22 18:51:22 +0530

    add image and h2 tag

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 const Header = () => {
+    return (
+        
+       <Header>
+         <img src="./Meme-Face-PNG-Picture.png" className="logo"/>
+         <h2>MeMe Generator</h2>
+       </Header>
+        
+    )
 
 }
 export default Header
